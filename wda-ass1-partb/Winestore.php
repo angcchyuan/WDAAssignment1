6bb71ace59fdd7293d18a79eb61ab10c01942fc8
angcchyuan <angcchyuan@gmail.com>
2014-10-12 21:46:54 +0800

    wda-ass1

---
+++ wda-ass1-partb/Winestore.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>Hi Welcome to WINESTORE </title>
+</head>
+
+<body>
+
+<?php
+$connection = mysqli_connect("localhost", "root", "", "winestore");
+if(mysqli_connect_errno())
+{
+	echo "Failed to connect to MySQL: ".mysqli_connect_error();
+}
+?>
+
+<h1>Hi Welcome to WINESTORE</h1>
+<form method="get" action="a.php">
+Search Wine Name:<div> <input type="text" name="wineName" id="wineName"><br><br></div>
+
+Search Region:<div> <?php
+					$result = mysqli_query($connection, "SELECT * FROM region");
+					echo "<select name='region' value=''>Dropdown</option>";
+					while ($row = mysqli_fetch_array($result))
+						echo "<option value = ".$row['region_name'].">".$row['region_name']."</option>";
+					echo"</select>";
+				?>
+<br><br></div>
+
+Search Winery Name: <div><input type="text" name="wineryName" id="wineryName"><br><br></div>
+
+Search Range of Years: <div>Starting <input type="text" name="sYear" id="sYear"></div>
+				<div>Ending &nbsp<input type="text" name="eYear" id="eYear"><br><br></div>
+
+Search Minimum number of Customer who have purchase each Wine: <div><input type = "text" name="cusNum" id="cusNum"><br><br></div>
+
+Search Minimum Number of Wines in Stock per Wine: <div><input type="text" name="stockNum" id="stockNum"><br><br></div>
+
+Search Dollar cost range: <div>MAX($): <input type="text" name="maxDollar" id="maxDollar"></div> 
+							<div>MIN($): <input type="text" name="minDollar" id="minDollar"><br><br></div>
+
+<input type="submit" value="Search" name="submit">
+
+</form>
+
+</body>
+
+</html>
---
+++ wda-ass1-partb/ssWinestore.php
@@ -0,0 +1,137 @@
+<?php
+
+	$connection = mysqli_connect("localhost", "root", "", "winestore");
+	
+	$wineName = $_GET["wineName"];
+	$region = $_GET["region"];
+	$wineryName = $_GET["wineryName"];
+	$sYear = $_GET["sYear"];
+	$eYear = $_GET["eYear"];
+	$cusNum = $_GET["cusNum"];
+	$stockNum = $_GET["stockNum"];
+	$maxDollar	= $_GET["maxDollar"];
+	$minDollar = $_GET["minDollar"];
+	
+	if($region == "ALL")
+		$region = 1;
+		
+	if($sYear == "")
+		$sYear = 0;
+		
+	if($eYear == "")
+		$eYear = 3000;
+		
+	if($cusNum == "")
+		$cusNum = 0;
+		
+	if($stockNum == "")
+		$stockNum = 0;
+	
+	if($maxDollar == "")
+		$maxDollar = 1000;
+		
+	if($minDollar == "")
+		$minDollar = 0;
+		
+	if((is_numeric($sYear))&&(is_numeric($eYear))&&
+		(is_numeric($cusNum))&&(is_numeric($stockNum)))
+	
+	{
+		if (!($sYear > $eYear)){
+		 if(!($minDollar > $maxDollar )){
+		
+			$test = ("SELECT wine.wine_id,wine.wine_name,wine.year,winery.winery_name,
+												region.region_name, wine_variety.variety_id,
+												grape_variety.variety, inventory.cost, inventory.on_hand,
+												COUNT(wine.wine_id) AS numOfCustomer
+										FROM wine
+										
+										INNER JOIN winery
+										ON wine.wine_id = winery.winery_id
+										
+										INNER JOIN region
+										ON winery.region_id = region.region_id
+										
+										INNER JOIN wine_variety
+										ON wine.wine_id = wine_variety.wine_id
+										
+										INNER JOIN grape_variety
+										ON wine_variety.variety_id = grape_variety.variety_id
+										
+										INNER JOIN inventory
+										ON wine.wine_id = inventory.wine_id
+										
+										INNER JOIN items
+										ON items.wine_id = wine.wine_id
+										
+										INNER JOIN customer
+										ON customer.cust_id = items.cust_id
+										
+										WHERE wine_name LIKE '%".$wineName."%'
+										AND winery_name LIKE '%".$wineryName."%'
+										AND inventory.on_hand >= '".$stockNum."'
+										AND wine.year >='".$sYear."'
+										AND wine.year <='".$eYear."'
+										AND inventory.cost >= '".$minDollar."'
+										AND inventory.cost <= '".$maxDollar."'
+										
+										GROUP BY wine.wine_id
+										
+										HAVING COUNT(wine.wine_id) >= '".$cusNum."'
+										"
+									);
+									
+		
+		
+
+		
+		
+			echo "<h>SEARCH RESULT(s)  </h><table border='1'>";
+			
+			echo "<tr>
+					<th>No.</th>
+					<th>WineName</th>
+					<th>Year</th>
+					<th>Winery</th>
+					<th>Region</th>
+					<th>WineVariety</th>
+					<th>Costing</th>
+					<th>No. of Stocks</th>
+					<th>No. of Customer Purchased</th>
+				</tr>";
+			$query = mysqli_query($connection, $test);
+			while($row = mysqli_fetch_array($query))
+			{
+				echo "<tr>
+						<td>".$row['wine_id']."</td>
+						<td>".$row['wine_name']."</td>
+						<td>".$row['year']."</td>
+						<td>".$row['winery_name']."</td>
+						<td>".$row['region_name']."</td>
+						<td>".$row['variety']."</td>
+						<td>".$row['cost']."</td>
+						<td>".$row['on_hand']."</td>
+						<td>".$row['numOfCustomer']."</td>
+					</tr>";
+			}
+			echo "</table>";
+		 }	
+		 
+		 else {	echo "Your minimum price($) " .$minDollar. " is bigger than your maximum price($) " .$maxDollar;}
+		 }
+		 
+		 else { echo "Your Starting year " .$sYear. " is bigger than your Eding Year " .$eYear; }
+	}
+	
+	else
+	{
+		echo "<h> NO Records is match your search!!</h>";
+		
+	}	
+	
+
+	
+?>
+	
+
+	
