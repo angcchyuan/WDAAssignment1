c844ddc8c075f0da4ba4628a85cbdbe472c22781
angcchyuan <angcchyuan@gmail.com>
2014-10-12 22:47:24 +0800

    partd

---
+++ wda-ass1-partd/Winestore.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+<title>Hi Welcome to WINESTORE </title>
+</head>
+
+<body>
+
+
+<h1>Hi Welcome to WINESTORE</h1>
+<form method="get" action="dd.php">
+Search Wine Name:<div> <input type="text" name="wineName" id="wineName"><br><br></div>
+
+Search Region:<div> <?php
+
+set_include_path('C:/wamp1/bin/php/php5.5.12/pear');
+	require_once "DB.php";
+ $username="root";
+	$password="";
+	$hostname="localhost";
+	$dbname="winestore";
+	$dsn="mysql://{$username}:{$password}@{$hostname}/{$dbname}";
+	
+	//when no database found
+	$connection = new DB();
+	$connection= @DB::connect($dsn);
+	if (@DB::isError($connection)) {
+    die("Unable to connect to database: " . $connection->getMessage() . "\n"
+                                          . $connection->getDebugInfo() . "\n");
+}
+
+
+	
+					
+					$result = @$connection->query("SELECT * FROM region");
+					echo "<select name='region' value=''>Dropdown</option>";
+					while ($row = $result->fetchRow())
+						echo "<option value = ".$row[1].">".$row[1]."</option>";
+					echo"</select>";
+				?>
+<br><br></div>
+
+Search Winery Name: <div><input type="text" name="wineryName" id="wineryName"><br><br></div>
+
+Search Range of Years: <div>Starting <input type="text" name="sYear" id="sYear"></div>
+				<div>Ending &nbsp<input type="text" name="eYear" id="eYear"><br><br></div>
+
+Search Minimum number of Customer who have purchase each Wine: <div><input type = "text" name="cusNum" id="cusNum"><br><br></div>
+
+Search Minimum Number of Wines in Stock per Wine: <div><input type="text" name="stockNum" id="stockNum"><br><br></div>
+
+Search Dollar cost range: <div>MAX($): <input type="text" name="maxDollar" id="maxDollar"></div> 
+							<div>MIN($): <input type="text" name="minDollar" id="minDollar"><br><br></div>
+
+<input type="submit" value="Search" name="submit">
+
+</form>
+
+</body>
+
+</html>
---
+++ wda-ass1-partd/test_template.tpl
@@ -0,0 +1,46 @@
+
+<html>
+<head>
+</head>
+
+<body>
+<table border="1" >
+
+<tr>
+<!-- BEGIN fieldName -->
+<td>{No.}</td>
+<td>{WineName}</td>
+<td>{Year}</td>
+<td>{Winery}</td>
+<td>{Region}</td>
+<td>{WineVariety}</td>
+<td>{Costing}</td>
+<td>{numOfStocks}</td>
+<td>{numOfPurchased}</td>
+</tr>
+<!-- END fieldName -->
+
+</tr>
+<tr>
+<!-- BEGIN result -->
+<td>{No.}</td>
+<td>{WineName}</td>
+<td>{year}</td>
+<td>{Winery}</td>
+<td>{Region}</td>
+<td>{WineVariety}</td>
+<td>{Costing}</td>
+<td>{numOfStocks}</td>
+<td>{numOfPurchased}</td>
+</tr>
+<!-- END result -->
+
+<!-- BEGIN noResult -->
+<td>{message}</td>
+</tr>
+<!-- END noResult -->
+
+</tr>
+</table>
+</body>
+</html>
